d4df84d304b0d58400052cfd36a85b09a2717323
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-09 14:44:58 +0530

    New Summarization method using nltk and networkx

--- summary.py
+++ summary.py
@@ -1,42 +1,83 @@
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.cluster.util import cosine_distance
+import numpy as np
+import networkx as nx
+ 
+def read_article(file_name):
+    file = open(file_name, "r")
+    filedata = file.readlines()
+    article = filedata[0].split(". ")
+    sentences = []
 
-text = "Humans (Homo sapiens) are the most abundant and widespread species of primate, characterized by bipedality, large and complex brains enabling the development of advanced tools, culture and language. Humans are highly social beings and tend to live in complex social structures composed of many cooperating and competing groups, from families and kinship networks to political states. Social interactions between humans have established a wide variety of values, social norms, and rituals, which bolster human society. Curiosity and the human desire to understand and influence the environment and to explain and manipulate phenomena have motivated humanity's development of science, philosophy, mythology, religion, and other fields of knowledge."
-
-stopWords = set(stopwords.words("english"))
-words = word_tokenize(text)
-
-freqTable = dict()
-
-for word in words:
-    word = word.lower()
-    if word in stopWords:
-        continue
-    if word in freqTable:
-        freqTable[word]+=1
-    else:
-        freqTable[word] = 1
-
-sentences = sent_tokenize(text)
-senteceValue = dict()
-for sen in sentences:
-    for word, freq in freqTable.items():
-        if word in sen.lower():
-            if sen in senteceValue:
-                senteceValue[sen] += 1
-            else:
-                senteceValue[sen] = 1
-sumValue = 0
-for sen in senteceValue:
-    sumValue += senteceValue[sen]
-
-average = int(sumValue/len(senteceValue))
-
-summary = ''
-
-for sen in sentences:
-    if(sen in senteceValue) and (senteceValue[sen]>(1.2*average)):
-        summary += " " + sen
-
-print(summary)
+    for sentence in article:
+        print(sentence)
+        sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+    sentences.pop() 
     
+    return sentences
+
+def sentence_similarity(sent1, sent2, stopwords=None):
+    if stopwords is None:
+        stopwords = []
+ 
+    sent1 = [w.lower() for w in sent1]
+    sent2 = [w.lower() for w in sent2]
+ 
+    all_words = list(set(sent1 + sent2))
+ 
+    vector1 = [0] * len(all_words)
+    vector2 = [0] * len(all_words)
+ 
+    # build the vector for the first sentence
+    for w in sent1:
+        if w in stopwords:
+            continue
+        vector1[all_words.index(w)] += 1
+ 
+    # build the vector for the second sentence
+    for w in sent2:
+        if w in stopwords:
+            continue
+        vector2[all_words.index(w)] += 1
+ 
+    return 1 - cosine_distance(vector1, vector2)
+ 
+def build_similarity_matrix(sentences, stop_words):
+    # Create an empty similarity matrix
+    similarity_matrix = np.zeros((len(sentences), len(sentences)))
+ 
+    for idx1 in range(len(sentences)):
+        for idx2 in range(len(sentences)):
+            if idx1 == idx2: #ignore if both are same sentences
+                continue 
+            similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+
+    return similarity_matrix
+
+
+def generate_summary(file_name, top_n=5):
+    stop_words = stopwords.words('english')
+    summarize_text = []
+
+    # Step 1 - Read text anc split it
+    sentences =  read_article(file_name)
+
+    # Step 2 - Generate Similary Martix across sentences
+    sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+
+    # Step 3 - Rank sentences in similarity martix
+    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+    scores = nx.pagerank(sentence_similarity_graph)
+
+    # Step 4 - Sort the rank and pick top sentences
+    ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
+    print("Indexes of top ranked_sentence order are ", ranked_sentence)    
+
+    for i in range(top_n):
+      summarize_text.append(" ".join(ranked_sentence[i][1]))
+
+    # Step 5 - Offcourse, output the summarize texr
+    print("Summarize Text: \n", ". ".join(summarize_text))
+
+# let's begin
+generate_summary( "msft.txt", 2)
