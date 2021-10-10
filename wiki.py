ae0842a15d07e74e048810981f9c41495a7ba49f
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-10 10:41:13 +0530

    Tested Jaccard Similarity metrix for calculating String similarity

--- wiki.py
+++ wiki.py
@@ -1,2 +1,15 @@
 import wikipedia
-print(wikipedia.summary("Humans"))
+
+def get_jaccard_sim(str1, str2): 
+    a = set(str1.split()) 
+    b = set(str2.split())
+    c = a.intersection(b)
+    return float(len(c)) / (len(a) + len(b) - len(c))
+
+keyword = "homo sapiens"
+search_results = wikipedia.search(keyword)
+score = dict()
+for i in search_results:
+    score[i] = get_jaccard_sim(keyword, i)
+
+print(score)
