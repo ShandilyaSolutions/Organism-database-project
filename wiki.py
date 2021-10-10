d6d872bba038b4d633b0ba360d806019c4dcb87f
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-10 11:47:59 +0530

    Finilasied the function to get highly accurate keyword

--- wiki.py
+++ wiki.py
@@ -2,13 +2,7 @@ import wikipedia
 from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import Levenshtein
-
-def get_jaccard_sim(str1, str2): 
-    a = set(str1.split()) 
-    b = set(str2.split())
-    c = a.intersection(b)
-    return float(len(c)) / (len(a) + len(b) - len(c))
+import json
 
 def get_cosine_sim(*strs): 
     vectors = [t for t in get_vectors(*strs)]
@@ -20,11 +14,22 @@ def get_vectors(*strs):
     vectorizer.fit(text)
     return vectorizer.transform(text).toarray()
 
-keyword = "reptile"
-search_results = wikipedia.search(keyword)
-score = dict()
-for i in search_results:
-    score[i] = get_cosine_sim(i, keyword)[0][1]
+def get_final_keyword(keyword):
+    search_results = wikipedia.search(keyword)
+    score_dict = dict()
+    score_list = list()
+    max_score = 0
+    for i in search_results:
+        score = get_cosine_sim(i, keyword)[0][1]
+        score_dict[i] = score
+        score_list.append(score)
+        max_score = max(max_score, score)
 
-print(score)
+    print(json.dumps(score_dict, indent=1))
+    for i in score_dict:
+        if score_dict[i] == max_score:
+            return i
+
+    
+print(get_final_keyword("naja naja"))
 
